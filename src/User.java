ab046b634f305626dc280b883242585fbb19286b
AliJafarli <somagni4icent@gmail.com>
2024-12-31 12:12:16 +0400

    1

---
+++ src/User.java
@@ -0,0 +1,41 @@
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    public User(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+
+    }
+    public int getId() {
+        return id;
+
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        if (id != user.id) return false;
+        if (age != user.age) return false;
+        return name != null ? name.equals(user.name) : user.name == null;
+
+    }
+
+}
